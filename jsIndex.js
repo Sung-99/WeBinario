3d9354017561010f948d42db79599ae495191422
Sung-99 <vitorhugomota99@hotmail.com>
2024-04-02 23:45:12 -0300

    up

--- jsIndex.js
+++ jsIndex.js
@@ -90,9 +90,41 @@ convr.addEventListener("click", () =>{
             xInitial++;
        }
        xInitial = 0;
-      
+       if(castNumb.length > 4  &&  castNumb.length%4 != 0){
+        let restKnow =  castNumb.length%4;
+        let contInnnerWhile = 0;
+        let anyCount=0;
+        
+        while(anyCount >= lengthMyBinNumber){
+            
+
+            if(lengthMyBinNumber%4 == 0){
+                while(contInnnerWhile < 4){
+                    lengthMyBinNumber--;
+                    if(castNumb[lengthMyBinNumber] == "1")result = result + (parseInt(castNumb[lengthMyBinNumber]) * (Math.pow(2, contt)));
+        
+                    contInnnerWhile++;
+                    
+                }
+                arrayToHex.push(result);
+                contInnnerWhile = 0;
+            }else{
+                while(contInnnerWhile < restKnow){
+                    lengthMyBinNumber--;
+                    if(castNumb[lengthMyBinNumber] == "1")result = result + (parseInt(castNumb[lengthMyBinNumber]) * (Math.pow(2, contt)));
+                    contInnnerWhile++;
+                }
+                contInnnerWhile = 0;
+               
+                arrayToHex.push(result);
+             }
+             
+             
+        }
+          
+       }else
        //See if my binary sequence is an exact divisions in 4 parts like: 1000 0101 1001 on 100001011001 (ex)
-          if(castNumb.length%4 == 0 || castNumb.length <= 4 ){
+          if(castNumb.length <= 4 || castNumb.length%4==0){
             lengthMyBinNumber =  castNumb.length;
             xInitial = 0;
             
@@ -114,25 +146,8 @@ convr.addEventListener("click", () =>{
                 contt = 0;  
                 xInitial = 0;  
             }
-          }else
-                if(castNumb.length > 4  &&  castNumb.length%4 != 0){
-                    let restKnow =  castNumb.length%4;
-
-                    let anyCount=0;
-                    let jump4 = 4;
-                    while(anyCount >= lengthMyBinNumber){
-
-                        if(lengthMyBinNumber%4 == 0)
-                        lengthMyBinNumber--;
-                    }
-                        console.log(castNumb[lengthMyBinNumber - restKnow]);
-
-                    
-                   
-                    
-                    
-                   // out.value = castNumb[lengthMyBinNumber];   
-                }
+          }
+          out.value = 'aa'; 
                 
          
        
@@ -142,7 +157,30 @@ convr.addEventListener("click", () =>{
        if(onlyBin > 0){
         out.value ='Apenas numeros 1s e 0s';
        }else
-            if(castNumb.length%4 == 0 || castNumb.length <= 4 ){
+            if(castNumb.length > 4  &&  castNumb.length%4 != 0){
+                for(let ccc = 0; ccc < arrayToHex.length; ccc++){
+                    if(arrayToHex[ccc] == 10){
+                        arrayToHex[ccc] = 'A';
+                    }else
+                    if(arrayToHex[ccc] == 11){
+                        arrayToHex[ccc] = 'B';
+                    }else
+                    if(arrayToHex[ccc] == 12){
+                        arrayToHex[ccc] = 'C';
+                    }else
+                    if(arrayToHex[ccc] == 13){
+                        arrayToHex[ccc] = 'D';
+                    }else
+                    if(arrayToHex[ccc] == 14){
+                        arrayToHex[ccc] = 'E';
+                    }else
+                    if(arrayToHex[ccc] == 15){
+                        arrayToHex[ccc] = 'F';
+                    }
+                }
+                out.value =  arrayToHex.toReversed().join("");       
+            }else
+                if(castNumb.length%4 == 0 || castNumb.length <= 4 ){
                     for(let ccc = 0; ccc < arrayToHex.length; ccc++){
                         if(arrayToHex[ccc] == 10){
                             arrayToHex[ccc] = 'A';
@@ -165,7 +203,8 @@ convr.addEventListener("click", () =>{
                     }
                     out.value =  arrayToHex.toReversed().join("");
                     
-                }
+                }  
+                    
        
       } 
 
