f5da99f85bc5a2517e431ca9857a1a76136f1cfb
Sung-99 <vitorhugomota99@hotmail.com>
2024-04-19 16:42:43 -0300

    up finish oct to hexa

--- jsIndex.js
+++ jsIndex.js
@@ -728,8 +728,200 @@ convr.addEventListener("click", () =>{
 
       if(options.selectedOptions[0].label == "Octal" && options2.selectedOptions[0].label == "Hexa" )
       {
+        /*-------------------------------octa to bin reuse--------------------*/
+        let check = false;
+        let restPlaces;
+        let RepeatPlaces;
+        let castedNumb = inputEntry.value;
+        let arrayToBin = [];
+        let finalArrayToBin = [];
+        let cont = 0;
+        let contPlaces = 0;
+        while(cont < castedNumb.length){
+            arrayToBin = [];
+            let timelyVariable = Number(castedNumb[cont]);
+          
+          if(timelyVariable == 8 || timelyVariable == 9){check = true; break;}  
+          if(timelyVariable>0){                              
+            while(timelyVariable >0){
+                
+                    
+                     
+                if(timelyVariable % 2 == 0) {
+                    //Easy solution***** using push and toReversed()//join() js functions
+                    arrayToBin.push(0);
+                    contPlaces++;
+               
+                }else{
+                    arrayToBin.push(1);
+                    contPlaces++;
+                
+                }
+                timelyVariable = Math.floor(timelyVariable/2);
+                
+            
+            }
+             restPlaces = 3 - contPlaces;
+             RepeatPlaces = 0;
+             while(RepeatPlaces < restPlaces){
+                arrayToBin.push(0); 
+                RepeatPlaces++;
+             }
+             contPlaces = 0;
+          }else{
+            let contador0 = 0;
+            while(contador0 <= 2){arrayToBin.push(0); contador0++;}
+          }
+          for(let xxx = 0; xxx < arrayToBin.length; xxx++)finalArrayToBin.push(arrayToBin.toReversed()[xxx]);
+          cont++;
+        }
         
-      }
+
+        /*-------------------------------octa to bin reuse end--------------------*/
+        
+        
+        /*-------------------------------bin to hexa reuse--------------------*/
+        
+       let onlyBin = 0;
+       let result = 0; 
+       let castNumb =finalArrayToBin;
+       let lengthMyBinNumber = castNumb.length ;
+       let xInitial = 0;
+       let arrayToHex = [];
+       let contt = 0;
+       let ocasionalCont = 0;
+       while(xInitial < castNumb.length){
+        
+        if(castNumb[xInitial] != "1" && castNumb[xInitial] != "0"){
+            onlyBin++;
+        }
+        
+            xInitial++;
+       }
+       xInitial = 0;
+         
+       if(castNumb.length > 4 && castNumb.length%4!=0){
+        
+        let contInnnerWhile = 0;
+        let anyCount=0;
+       
+        while(anyCount < lengthMyBinNumber){
+            
+           // result = castNumb[0];
+            if(lengthMyBinNumber%4 != 0){
+                while(contInnnerWhile < 4){
+                    
+                    lengthMyBinNumber--;
+                    if(lengthMyBinNumber < 0)break;
+                    if(castNumb[lengthMyBinNumber] == "1")result = result + (parseInt(castNumb[lengthMyBinNumber]) * (Math.pow(2, contt)));
+                    contt++;
+                    
+                    contInnnerWhile++;
+                    
+                }
+                arrayToHex.push(result);
+                contt= 0;
+                contInnnerWhile = 0;
+                result =0;
+            }
+             
+             
+        }
+       
+       }else
+       //See if my binary sequence is an exact quotioent in 4 parts like: 1000 0101 1001 on 100001011001 (ex)
+          if(castNumb.length <= 4 || castNumb.length%4==0){
+            lengthMyBinNumber =  castNumb.length;
+            xInitial = 0;
+            
+           
+            while(castNumb.length > ocasionalCont){
+
+                
+                    xInitial = xInitial + 4;
+                    ocasionalCont = ocasionalCont + 4;
+                while(contt < xInitial){
+                    lengthMyBinNumber--;
+                    if(castNumb[lengthMyBinNumber] == "1")result = result + (parseInt(castNumb[lengthMyBinNumber]) * (Math.pow(2, contt)));
+                    
+                    contt++;
+                   
+                }
+                arrayToHex.push(result);
+                result = 0;
+                contt = 0;  
+                xInitial = 0;  
+            }
+          }
+       if(check){
+            out.value = 'Só numeros entre 0s e 7s';
+            }else
+                if(castNumb.length > 4  &&  castNumb.length%4 != 0){
+                  for(let ccc = 0; ccc < arrayToHex.length; ccc++){
+                    if(arrayToHex[ccc] == 10){
+                        arrayToHex[ccc] = 'A';
+                    }else
+                    if(arrayToHex[ccc] == 11){
+                        arrayToHex[ccc] = 'B';
+                    }else
+                    if(arrayToHex[ccc] == 12){
+                        arrayToHex[ccc] = 'C';
+                    }else
+                    if(arrayToHex[ccc] == 13){
+                        arrayToHex[ccc] = 'D';
+                    }else
+                    if(arrayToHex[ccc] == 14){
+                        arrayToHex[ccc] = 'E';
+                    }else
+                    if(arrayToHex[ccc] == 15){
+                        arrayToHex[ccc] = 'F';
+                    }
+                  }
+                    if(arrayToHex.toReversed()[1] > 0 && arrayToHex.toReversed()[0] == 0){
+                        arrayToHex.pop();//remove the zero when the most left number is 0(after all calculations like *0*00111010 or other number)
+                        //or other number in hexa to bin like 321(16)  = 001100100001(2) the first number is 0
+                   
+                        out.value  = arrayToHex.toReversed().join("");
+                    }else{
+                        out.value = arrayToHex.toReversed().join("");
+                    }
+                         
+                    
+                }else
+                  if(castNumb.length%4 == 0 || castNumb.length <= 4 ){
+                    for(let ccc = 0; ccc < arrayToHex.length; ccc++){
+                        if(arrayToHex[ccc] == 10){
+                            arrayToHex[ccc] = 'A';
+                        }else
+                        if(arrayToHex[ccc] == 11){
+                            arrayToHex[ccc] = 'B';
+                        }else
+                        if(arrayToHex[ccc] == 12){
+                            arrayToHex[ccc] = 'C';
+                        }else
+                        if(arrayToHex[ccc] == 13){
+                            arrayToHex[ccc] = 'D';
+                        }else
+                        if(arrayToHex[ccc] == 14){
+                            arrayToHex[ccc] = 'E';
+                        }else
+                        if(arrayToHex[ccc] == 15){
+                            arrayToHex[ccc] = 'F';
+                        }
+                    }
+                    if(arrayToHex.toReversed()[1] > 0 && arrayToHex.toReversed()[0] == 0){
+                        arrayToOct.pop();//remove the zero when the most left number is 0(after all calculations like *0*00111010 or other number)
+                        //or other number in hexa to bin like 321(16)  = 001100100001(2) the first number is 0
+                   
+                        out.value  = arrayToHex.toReversed().join("");
+                    }else{
+                        out.value = arrayToHex.toReversed().join("");
+                    }
+                    
+                }
+        /*-------------------------------bin to hexa reuse end--------------------*/ 
+    
+    }
 })
 
 Limpar.addEventListener("click", () =>{
